374bb7655eae9b898b8bf211dd3c4e2bdf5cc5db
private101st <matteo.albertinioffeddu@gmail.com>
2025-01-26 11:32:52 +0100

    inizio implementazione testo finale

--- game jam 2025/scenes/EndBubbleText.gd
+++ game jam 2025/scenes/EndBubbleText.gd
@@ -1,3 +1,26 @@
 extends Label
-#func _ready():
-#	if WorldState.economic_sit < 
+func _ready():
+	var text = ""
+	var climate = WorldState.climate
+	var tech = WorldState.tech
+	var int_stab = WorldState.int_stab
+	var unrest = WorldState.unrest
+	var might = WorldState.might
+	var bubble_res = WorldState.bubble_res
+	var bubble_akn = WorldState.bubble_akn
+	var economic_sit = WorldState.economic_sit
+func techcheck(tech, text):
+	if tech < 3:
+		text += "Your bubble has not seen any technological progress, and is still stuck in the stone age."
+	elif tech < 6 and tech > 2:
+		text += "Your bubble has seen some technological progress, and is now developing it's first technologies, such as gunpowder and modern industry."
+	elif tech < 8 and tech > 5:
+		text += "Your bubble has seen a lot of technological progress, and is now using modern computers and guns."
+	elif tech > 7:
+		text += "Your bubble has reached an unseen new level of technological progress; it could very well destroy the bubble, if it wanted to..."
+	return text
+func climatecheck(climate, text):
+	if climate < -10:
+		text += "Your bubble is now freezing, and life has become almost impossible for its inhabitants."
+	if climate < 10 and climate > -9:
+		text += "Your bubble has become really cold, and life is very harsh for its inhabitants."	
