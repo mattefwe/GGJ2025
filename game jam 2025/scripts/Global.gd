5fceb83f0864b764af44c78761a7410ab378755a
mattefwe <matterisco@gmail.com>
2025-01-26 09:25:21 +0100

    .

--- game jam 2025/scripts/Global.gd
+++ game jam 2025/scripts/Global.gd
@@ -1,5 +1,8 @@
 extends Node
 
+# changing the music variable plays a different background music
+# depending on the number you set it to
+#for example: 1 is th title screen music and 2 is the in game music
 var music = 0
 var et_on = 0
 var opt_on = 0
